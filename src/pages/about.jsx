396607824099d84e9324fe6f1220ccebd0409339
Jordan Johnson <jordaninjohnson@gmail.com>
2020-08-16 15:22:42 -0600

    updated

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
-        </div>
+        <>
+            <div className="jumbotron jumbotron-fluid bg-dark">
+                <div className="container">
+                    <h1 className="display-4 ">Jordan Johnson</h1>
+                    <p className="lead">Full-Stack Web Developer</p>
+                </div>
+            </div>
+        </>
     );
 }
