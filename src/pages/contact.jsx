f3243f6261f74b23d40597f8c6e54c5cfe85b54e
Jordan Johnson <jordaninjohnson@gmail.com>
2020-08-16 14:55:14 -0600

    updated

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,30 +3,30 @@ import React from 'react'
 export default function Contact() {
     return (
         <div className="container">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-sm d-flex justify-content-sm-center">
-                        <h2 class="py-3">Connect With Me Anytime!</h2>
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-sm d-flex justify-content-sm-center">
+                        <h2 className="py-3">Connect With Me Anytime!</h2>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-6 container justify-content-center">
+                <div className="row">
+                    <div className="col-6 container justify-content-center">
                         <form action="https://formspree.io/mnqgpjvd" method="POST">
-                            <div class="form-group">
-                                <label for="userName">Name</label>
-                                <input type="text" class="form-control" id="userName" name="name" placeholder="Name" />
+                            <div className="form-group">
+                                <label htmlFor="userName">Name</label>
+                                <input type="text" className="form-control" id="userName" name="name" placeholder="Name" />
                             </div>
-                            <div class="form-group">
-                                <label for="email">Email address</label>
-                                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            <div className="form-group">
+                                <label htmlFor="email">Email address</label>
+                                <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                                     placeholder="Email" />
                             </div>
-                            <div class="form-group">
-                                <label for="textArea">Your Message</label>
-                                <textarea class="form-control" name="message" id="message" rows="6" placeholder="Message" />
+                            <div className="form-group">
+                                <label htmlFor="textArea">Your Message</label>
+                                <textarea className="form-control" name="message" id="message" rows="6" placeholder="Message" />
                             </div>
-                            <div class="col-sm d-flex justify-content-sm-center">
-                                <button type="submit" class="btn btn-light">Submit</button>
+                            <div className="col-sm d-flex justify-content-sm-center">
+                                <button type="submit" className="btn btn-light">Submit</button>
                             </div>
                         </form>
                     </div>
