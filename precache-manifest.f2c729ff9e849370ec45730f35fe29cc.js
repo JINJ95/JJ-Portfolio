eea295b7c5a4602e5f1534e1b8a4c66fb09bc7d8
Jordan Johnson <jordaninjohnson@gmail.com>
2020-08-19 23:21:02 -0600

    Updates

---
+++ precache-manifest.f2c729ff9e849370ec45730f35fe29cc.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95fa5533bf584456e18a42d32f1cfb2",
+    "url": "/JJ-Portfolio/index.html"
+  },
+  {
+    "revision": "8053d38c09534e3dd644",
+    "url": "/JJ-Portfolio/static/css/main.3c48443c.chunk.css"
+  },
+  {
+    "revision": "cce6044d5cc44e931cb9",
+    "url": "/JJ-Portfolio/static/js/2.8725717b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JJ-Portfolio/static/js/2.8725717b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8053d38c09534e3dd644",
+    "url": "/JJ-Portfolio/static/js/main.e08c0300.chunk.js"
+  },
+  {
+    "revision": "0e1f14a713c0744cb002",
+    "url": "/JJ-Portfolio/static/js/runtime-main.73667d9d.js"
+  },
+  {
+    "revision": "4544f2fd28ac36ff9b4fab1d9bde3561",
+    "url": "/JJ-Portfolio/static/media/Budget-Tracker.4544f2fd.png"
+  },
+  {
+    "revision": "9a5235a6eaa811cce04f0a1bd2c7d453",
+    "url": "/JJ-Portfolio/static/media/Certificate.9a5235a6.pdf"
+  },
+  {
+    "revision": "61baebe6688d3b6795881a1c43be4210",
+    "url": "/JJ-Portfolio/static/media/Jordan Johnson Resume 2020.61baebe6.pdf"
+  },
+  {
+    "revision": "ec5d9de30a5e8dfad13fcb682ac7a844",
+    "url": "/JJ-Portfolio/static/media/KarimNI.ec5d9de3.JPG"
+  },
+  {
+    "revision": "9f0d974bb8ccbbb24c92657e6b944a9f",
+    "url": "/JJ-Portfolio/static/media/NewPasswordGenerator.9f0d974b.png"
+  },
+  {
+    "revision": "5838504684068029313254ba418c0cab",
+    "url": "/JJ-Portfolio/static/media/NewWeatherDashboard.58385046.png"
+  },
+  {
+    "revision": "431ac061d017e0e12cdd6c8f9c07ec19",
+    "url": "/JJ-Portfolio/static/media/NewWorkDayScheduler.431ac061.png"
+  },
+  {
+    "revision": "34d3ebada501758098f2b59fe14d0126",
+    "url": "/JJ-Portfolio/static/media/Powell.34d3ebad.jpeg"
+  },
+  {
+    "revision": "55900f3ab43b523e69a3ccbda85eda27",
+    "url": "/JJ-Portfolio/static/media/Sky&I.55900f3a.JPG"
+  },
+  {
+    "revision": "94b629dc69db27f1ce17a66038c4ff10",
+    "url": "/JJ-Portfolio/static/media/WorkoutTracker.94b629dc.png"
+  },
+  {
+    "revision": "c0852bbe8c8717fe5925ee59f2161b66",
+    "url": "/JJ-Portfolio/static/media/burgers.c0852bbe.png"
+  },
+  {
+    "revision": "e42842f2f5763a611f53ac322a04eff1",
+    "url": "/JJ-Portfolio/static/media/codingQuiz.e42842f2.png"
+  },
+  {
+    "revision": "1b1e504b9034d7abc478e4d46c640bae",
+    "url": "/JJ-Portfolio/static/media/colorWaves.1b1e504b.png"
+  },
+  {
+    "revision": "724f1a2bbb5344cec7170bd650f469ee",
+    "url": "/JJ-Portfolio/static/media/dogs.724f1a2b.JPG"
+  },
+  {
+    "revision": "ace98da34c1d162fa11aa4100354aaa8",
+    "url": "/JJ-Portfolio/static/media/employeeDirectory.ace98da3.png"
+  },
+  {
+    "revision": "e74595c0f683271a5bdabe5ab6d6940f",
+    "url": "/JJ-Portfolio/static/media/employeeTracker.e74595c0.png"
+  },
+  {
+    "revision": "13acc3c05d5a46cf2ad70c1b2be0a54c",
+    "url": "/JJ-Portfolio/static/media/googleBooks.13acc3c0.png"
+  },
+  {
+    "revision": "d365b5f52919bfc4dc01acc80e944bce",
+    "url": "/JJ-Portfolio/static/media/lake.d365b5f5.JPG"
+  },
+  {
+    "revision": "1fd50f9b78bd74eba28e5b0be67e9309",
+    "url": "/JJ-Portfolio/static/media/myAutoSpace.1fd50f9b.png"
+  },
+  {
+    "revision": "30f2a1bc99b33ed81600902437600711",
+    "url": "/JJ-Portfolio/static/media/noteTaker.30f2a1bc.gif"
+  },
+  {
+    "revision": "b681300a5fe105f30af84cd1d56934c0",
+    "url": "/JJ-Portfolio/static/media/readmeGenerator.b681300a.gif"
+  },
+  {
+    "revision": "842c4d4daa56fc42fd610e59efb0d541",
+    "url": "/JJ-Portfolio/static/media/richfield.842c4d4d.JPG"
+  },
+  {
+    "revision": "56023034c30f85b6ce9e8554e89843b1",
+    "url": "/JJ-Portfolio/static/media/snow.56023034.JPG"
+  },
+  {
+    "revision": "b7cffc56bb772746131ce717131ce9e4",
+    "url": "/JJ-Portfolio/static/media/strtup.b7cffc56.png"
+  },
+  {
+    "revision": "271f90a00e5dbeaf24901da75e8d7f23",
+    "url": "/JJ-Portfolio/static/media/teamSummary.271f90a0.gif"
+  },
+  {
+    "revision": "3425ed340389b2d3bff8408ce6b9f58f",
+    "url": "/JJ-Portfolio/static/media/water.3425ed34.jpeg"
+  }
+]);
