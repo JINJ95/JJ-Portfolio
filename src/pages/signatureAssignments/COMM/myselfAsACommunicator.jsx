5ae331142b93f600d65b664d494c8e2309699168
Jordan Johnson <jordaninjohnson@gmail.com>
2020-12-16 12:28:22 -0700

    comm1010 e-portfolio

--- src/pages/signatureAssignments/COMM/myselfAsACommunicator.jsx
+++ src/pages/signatureAssignments/COMM/myselfAsACommunicator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Overview from '../../../assets/pictures/CommEportfolioSignatureAssignmentOverview.pdf'
 import { withRouter } from 'react-router-dom'
 import Jumbotron from '../../../components/Jumbotron'
+import myselfCommunicator from '../../../assets/pictures/MyselfAsACommunicator.pdf'
 
 function Myself() {
     return (
@@ -12,75 +13,9 @@ function Myself() {
                 <iframe title="Comm" src={Overview} width="100%" height="800" style={{ border: "none" }} />
             </div>
             <div class="media-body">
-                <Jumbotron title="Myself As a Communicator" subtitle="ePortfolio Signature Assignment: Reflections / Essay" />
-                <h5 class="mt-0"><p>For this part of my ePortfolio project I submitted a collection of reflections on some of my communication skills
-                I learned about in this class. These reflection prompts have been assigned throughout Fall semester of 2020.
-                </p>
-                </h5>
-                <div id="reflections">
-                    <div class="accordion" id="accordionExample">
-                        <div class="card">
-                            <div class="card-header" id="headingOne">
-                                <h2 class="mb-0">
-                                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        Reflection #1: Perception and Self
-                                    </button>
-                                </h2>
-                            </div>
-
-                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                                <div class="card-body">
-                                Reflection: Perception and Self
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-header" id="headingTwo">
-                                <h2 class="mb-0">
-                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Reflection #2: Verbal Communication
-                                    </button>
-                                </h2>
-                            </div>
-                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    Reflection 2 Verbal Communication
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-header" id="headingThree">
-                                <h2 class="mb-0">
-                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Reflection #3: Listening
-                                    </button>
-                                </h2>
-                            </div>
-                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    Listening Reflection
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-header" id="headingThree">
-                                <h2 class="mb-0">
-                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Reflection #4: Nonverbal Communication
-                                    </button>
-                                </h2>
-                            </div>
-                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-                                <div class="card-body">
-                                    Nonverbal Communication Reflection
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Jumbotron title="Myself as a Communicator Essay" subtitle="by: Jordan Johnson" />
+                <Jumbotron title="Myself as a Communicator" subtitle="by: Jordan Johnson" />
             <div class="media">
-                <iframe title="Comm" src={Overview} width="100%" height="800" style={{ border: "none" }} />
+                <iframe title="Comm" src={myselfCommunicator} width="100%" height="800" style={{ border: "none" }} />
             </div>
             </div>
         </div>
